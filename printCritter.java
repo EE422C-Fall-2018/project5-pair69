91bdf5056192c3568d0e11410a7d53cbd76a1e46
kaylantran <kaylatran@utexas.edu>
2018-11-11 17:22:25 -0600

    Add files via upload

--- printCritter.java
+++ printCritter.java
@@ -1,13 +1,19 @@
 package application;
 
+import assignment5.Params;
 import assignment5.Critter.CritterShape;
-import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
-import javafx.stage.Stage;
+
 
 public class printCritter {
 
@@ -50,5 +56,11 @@ public class printCritter {
 		CritterWorld.world.add(s, x, y);
 		return null;
 	}
-
+	public static void clearDisplay() {
+		Node node = CritterWorld.world.getChildren().get(0);
+		CritterWorld.world.getChildren().clear();
+		CritterWorld.world.getChildren().add(0,node);
+		CritterWorld.world.setStyle("-fx-background-color: white; -fx-grid-lines-visible: true");
+		
+	}
 }
