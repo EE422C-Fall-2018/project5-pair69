285c77e29f5f2727dd7447f4c261dd50b7b98eff
arvinbhatti <arvinbhatti@gmail.com>
2018-11-12 22:26:06 -0600

    Add files via upload

--- CritterWorld.java
+++ CritterWorld.java
@@ -1,38 +1,35 @@
 package application;
 
-import java.util.List;
-
 import java.io.File;
-import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 import assignment5.Critter;
 import assignment5.Params;
 import javafx.animation.KeyFrame;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-
+import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.concurrent.Task;
+import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
-import javafx.scene.media.AudioClip;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.paint.Color;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BackgroundPosition;
@@ -41,9 +38,15 @@ import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
-
+import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -52,7 +55,6 @@ public class CritterWorld extends Application {
 	public static GridPane world = new GridPane();
 	static BorderPane bp = new BorderPane();
 	static Timeline timeline  = new Timeline();
-
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -61,37 +63,36 @@ public class CritterWorld extends Application {
 		GridPane grid = new GridPane();
 		grid.setHgap(5);
 		grid.setVgap(5);
-		grid.setPadding(new Insets(5,5,5,5));
+		grid.setPadding(new Insets(10,10,10,10));
 		grid.setGridLinesVisible(false);
-		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/assignment5/song.wav").getAbsoluteFile());
-		Clip clip = AudioSystem.getClip();
-		clip.open(audioInputStream);
 		
-		world.setMinSize(Params.world_width , Params.world_height);
-		world.setMaxSize(Params.world_width*10, Params.world_height*10);
-
+		
+		//gridPane.setMinSize(Params.world_width + 30 , Params.world_height+ 30);
+//		Button button1 = new Button("Button 1");
+//		Button button2 = new Button("Button 2");
+//		grid.add(button2, 0, 0);
+//		grid.add(button1, 0, 2);
 		BackgroundImage myBI= new BackgroundImage(new Image("http://www.ece.utexas.edu/sites/default/files/portraits/VNK2016_smaller.jpg",1250,1000,false,true),
                 BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                   BackgroundSize.DEFAULT);
         
-        world.setBackground(new Background(myBI));
+        CritterWorld.world.setBackground(new Background(myBI));
 		world.setStyle("-fx-grid-lines-visible: true");
-		world.setPadding(new Insets(5,5,5,5));
-
+		world.setPadding(new Insets(10,10,10,10));
+		//world.setGridLinesVisible(true);
 		for (int i = 0; i < Params.world_width; i++) {
            	ColumnConstraints cc = new ColumnConstraints();
            	cc.setPercentWidth(100.0 / Params.world_width);
            	cc.setHalignment(HPos.CENTER);
           	world.getColumnConstraints().add(cc);
         }
-		
         for (int i = 0; i < Params.world_height; i++) {
            	RowConstraints rc = new RowConstraints();
            	rc.setPercentHeight(100.0 / Params.world_height);
            	rc.setValignment(VPos.CENTER);
         	world.getRowConstraints().add(rc);
         }
- 
+        
         //make critter button
     	
         TextField makeField = new TextField ();
@@ -108,45 +109,42 @@ public class CritterWorld extends Application {
         //step button
         Button step = new Button();
         step.setText("Step");
-        grid.add(step, 4, 0);
-
+        grid.add(step, 1, 1);
+        //Label steplabel = new Label("Number of Steps:");
         TextField stepField = new TextField ();
         stepField.setPromptText("Enter # of Steps");
-   
-        grid.add(stepField, 3, 0);
+        //grid.add(steplabel, 0, 2);
+        grid.add(stepField, 0, 1);
         
         //seed button
         Button seed = new Button();
         seed.setText("Set Seed");
-        grid.add(seed, 6, 0);
+        grid.add(seed, 1, 2);
         TextField seedField = new TextField ();
         seedField.setPromptText("Enter Seed Number");
-        grid.add(seedField, 5, 0);
+        grid.add(seedField, 0, 2);
         
         //run stats button
         Button stat = new Button();
         stat.setText("Run Stats");
-        grid.add(stat, 1, 1);
+        grid.add(stat, 1, 4);
         TextField statField = new TextField ();
         statField.setPromptText("Enter Critter Class");
-        grid.add(statField, 0, 1);
+        grid.add(statField, 0, 4);
         
-        TextField statbox = new TextField();
-
-        statbox.setPromptText("Critter Stats will appear here");
-        grid.add(statbox,2,1,6,1);
+        TextArea statbox = new TextArea();
+        //VBox vbox = new VBox(statbox);
+        //statbox.setPrefColumnCount(2);
+        grid.add(statbox,0,5,2,1);
         
         //quit button
         Button end = new Button();
         end.setText("Quit");
-        grid.add(end, 2, 2);
+        grid.add(end, 0, 8);
         
-        //animation button
         Button animation = new Button();
         animation.setText("Animate");
-        grid.add(animation, 8, 0);
-        
-        //slider for animation
+        grid.add(animation, 0, 7);
         Slider slider = new Slider();
         slider.setMin(1);
         slider.setMax(100);
@@ -156,30 +154,16 @@ public class CritterWorld extends Application {
         slider.setMajorTickUnit(49);
         slider.setMinorTickCount(5);
         slider.setBlockIncrement(10);
-        grid.add(slider, 7, 0);
+        grid.add(slider, 0, 6);
         Button stop = new Button();
-        
-        //stop animation button
-        stop.setText("Stop Animation");
-        grid.add(stop, 9, 0);
+        stop.setText("Stop");
+        grid.add(stop, 1, 7);
         stop.setDisable(true);
         
-        //music button
         Button music = new Button();
         music.setText("Music");
-        grid.add(music, 0, 2);
-        
-        //stop music button
-        Button stopmusic = new Button();
-        stopmusic.setText("Stop Music");
-        grid.add(stopmusic, 1,2);
-        stopmusic.setDisable(true);
-        
-        //reset button
-        Button reset = new Button();
-        reset.setText("Reset");
-        grid.add(reset, 3, 2);
-        
+        grid.add(music, 0, 8);
+
         make.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
@@ -212,13 +196,13 @@ public class CritterWorld extends Application {
 			public void handle(ActionEvent event) {
 				if(stepField.getText().isEmpty()) {
 					Critter.worldTimeStep();
-					System.out.println("stepped 1 time");
+					//System.out.println("stepped 1 time");
     			}else {
     				try {
     					int steps = Integer.parseInt(stepField.getText());
         				for (int i = 0; i < steps; i++) {
         					Critter.worldTimeStep();
-        					System.out.println("stepped multiple times");
+        					//System.out.println("stepped multiple times");
         				}
     				}
     				catch(Exception e){
@@ -273,16 +257,15 @@ public class CritterWorld extends Application {
         	}
         });
         
-        
-        
         music.setOnAction(new EventHandler<ActionEvent>() {
         	@Override
         	public void handle(ActionEvent event) {
         		
         		try {
+        	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/assignment5/song1.wav").getAbsoluteFile());
+        	        Clip clip = AudioSystem.getClip();
+        	        clip.open(audioInputStream);
         	        clip.start();
-        	        music.setDisable(true);
-        	        stopmusic.setDisable(false);
         	    } catch(Exception ex) {
         	        System.out.println("Error with playing sound.");
         	        ex.printStackTrace();
@@ -291,21 +274,6 @@ public class CritterWorld extends Application {
               	}
         });
         
-        stopmusic.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent event) {
-        		
-        		try {   
-        	      clip.stop();
-        	      music.setDisable(false);
-        	      stopmusic.setDisable(true);
-        	    } catch(Exception ex) {
-        	        System.out.println("Error with playing sound.");
-        	        ex.printStackTrace();
-        	    }
-                
-              	}
-        });
         
         stop.setOnAction(new EventHandler<ActionEvent>() {
         	@Override
@@ -323,12 +291,8 @@ public class CritterWorld extends Application {
         		end.setDisable(false);
         		animation.setDisable(false);
         		stop.setDisable(true);
-        		slider.setDisable(false);
-        		music.setDisable(false);
-        		stopmusic.setDisable(false);
-        		reset.setDisable(false);
         		timeline.stop();
-        		System.out.println("Stopped");
+        		//System.out.println("Stopped");
         	}
         });
         
@@ -349,10 +313,6 @@ public class CritterWorld extends Application {
         		end.setDisable(true);
         		animation.setDisable(true);
         		stop.setDisable(false);
-        		slider.setDisable(true);
-        		music.setDisable(true);
-        		stopmusic.setDisable(true);
-        		reset.setDisable(true);
         		
         		KeyFrame keyFrame  = new KeyFrame(Duration.millis(1000), new EventHandler<ActionEvent>() {
         	        	@Override
@@ -372,38 +332,40 @@ public class CritterWorld extends Application {
         	}
         });
         
-        reset.setOnAction(new EventHandler<ActionEvent>() {
-        	@Override
-        	public void handle(ActionEvent event) {
-        		try {
-        			clip.stop();
-        			Clip clip = AudioSystem.getClip();
-					clip.open(audioInputStream);
-				} catch (LineUnavailableException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-        		Critter.clearWorld();
-        		Critter.updateDisplay();
-                
-              	}
-        });
         
         
         
         //BorderPane.setAlignment(world, Pos.CENTER);
         //BorderPane.setAlignment(grid, Pos.TOP_RIGHT);
-        BorderPane bp = new BorderPane(world, null, null, grid, null);
-	
-        Scene scene = new Scene(bp,300,250);
-		primaryStage.setScene(scene);
+        BorderPane bp = new BorderPane(world, null, grid, null, null);
+		//gridPane.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		
+	    
+
+//	    final Task task = new Task() {
+//
+//	        protected Object call() throws Exception {
+//	            int s = 2147483647;
+//	            AudioClip audio = new AudioClip(getClass().getResource("Schoolboy Q -  Hell Of A Night.wav").toExternalForm());
+//	            audio.setVolume(0.5f);
+//	            audio.setCycleCount(s);
+//	            audio.play();
+//	            return null;
+//	        }
+//	    };
+//	    Thread thread = new Thread(task);
+//	    thread.start();
+	    primaryStage.setScene(new Scene(bp,300,250));
 		primaryStage.show();
-		
-		
-		
+		try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/assignment5/song1.wav").getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
 	
 	}
 
